83c529febce07be4f684d91c9c9965768eadec7a
Mike Clarke <clarkema@clarkema.org>
2024-01-13 16:23:35 +0000

    Rough test

---
+++ default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> { system = builtins.currentSystem; }
+, lib ? pkgs.lib
+}:
+
+pkgs.stdenv.mkDerivation {
+  name = "nix-rtl-test";
+  src = ./.;
+
+  postInstall = ''
+    mkdir -p $out/etc/udev/rules.d
+    cp dist/nix/99-rtl-sdr.rules $out/etc/udev/rules.d/99-rtl-sdr.rules
+  '';
+}
